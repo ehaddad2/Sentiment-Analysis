81e1d0967362213f3a2988a5d12509833045b24e
anastch03 <anastasiac1203@gmail.com>
2024-11-22 17:29:04 -0500

    apply all cleaning to dataset

--- clean_data.py
+++ clean_data.py
@@ -16,18 +16,10 @@ def normalize_case(text):
     text = text.lower()
     return text
 
-case_normalized = copy.deepcopy(IMDB_dataset)
-case_normalized['review'] = case_normalized['review'].apply(normalize_case)
-case_normalized.to_csv("case_normalized.csv", index=False)
-
 #2. Punctuation Removal
 def remove_punctuation(text):
     text = text.translate(text.maketrans("", "", string.punctuation))
     return text
-
-no_punctuation = copy.deepcopy(IMDB_dataset)
-no_punctuation['review'] = no_punctuation['review'].apply(remove_punctuation)
-no_punctuation.to_csv("no_punctuation.csv", index=False)
 
 
 #3. Stop Word Removal
@@ -38,10 +30,6 @@ def remove_stop(text):
     text = " ".join(filtered_words)
     return text
 
-no_stop = copy.deepcopy(IMDB_dataset)
-no_stop['review'] = no_stop['review'].apply(remove_stop)
-no_stop.to_csv("no_stop.csv", index=False)
-
 #4. Stemming
 def stemming(text):
     stemmer = PorterStemmer()
@@ -50,10 +38,6 @@ def stemming(text):
     text = ",".join(stemmed_words)
     return text
 
-stemmed = copy.deepcopy(IMDB_dataset)
-stemmed['review'] = stemmed['review'].apply(stemming)
-stemmed.to_csv("stemmed.csv", index=False)
-
 #5. Lemmatization
 def lemmatize(text):
     lemmatizer = WordNetLemmatizer()
@@ -61,35 +45,33 @@ def lemmatize(text):
     lemmatized_words = [lemmatizer.lemmatize(word) for word in words]
     text = ",".join(lemmatized_words)
     return text
-    
-lemmatized = copy.deepcopy(IMDB_dataset)
-lemmatized['review'] = lemmatized['review'].apply(lemmatize)
-lemmatized.to_csv("lemmatized.csv", index=False)
 
 #6. Removing numbers and symbols
 def remove_num_symbols(text):
     text = re.sub(r"[\d#]", "", text)
     return text 
 
-no_num_symbols = copy.deepcopy(IMDB_dataset)
-no_num_symbols['review'] = no_num_symbols['review'].apply(remove_num_symbols)
-no_num_symbols.to_csv("no_num_symbols.csv", index=False)
-
 #7. removing non-text elements
 def remove_non_text(text):
     text = re.sub(r"(<[^>]+>)|(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)", "", text)
     return text
-
-no_non_text = copy.deepcopy(IMDB_dataset)
-no_non_text['review'] = no_non_text['review'].apply(remove_non_text)
-no_non_text.to_csv("no_non_text.csv", index=False)
 
 #8. Removing extra spaces
 def remove_extra_space(text):
     text = re.sub(r"\s+", " ", text)
     return text
 
-no_extra_space = copy.deepcopy(IMDB_dataset)
-no_extra_space['review'] = no_extra_space['review'].apply(remove_extra_space)
-no_extra_space.to_csv("no_extra_space.csv", index=False)
+def clean_text(text):
+    text = normalize_case(text)
+    text = remove_punctuation(text)
+    text = remove_stop(text)
+    text = stemming(text)
+    text = lemmatize(text)
+    text = remove_num_symbols(text)
+    text = remove_non_text(text)
+    text = remove_extra_space(text)
+    return text
 
+cleaned = copy.deepcopy(IMDB_dataset)
+cleaned['review'] = cleaned['review'].apply(clean_text)
+cleaned.to_csv("cleaned_IMBD_dataset.csv", index=False)
--- project.py
+++ project.py
@@ -1,8 +1,3 @@
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load
-
-
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import torch
@@ -25,6 +20,7 @@ import re
 import copy
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print(f'\nDevice being used: ', device, '\n')
+
 SEED = 30
 EPOCHS = 10
 torch.manual_seed(SEED)
